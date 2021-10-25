b90184a630560c7fd45dabdc94038320ac559065
lizashaab <93035171+lizashaab@users.noreply.github.com>
2021-10-25 14:45:05 +0300

    Add files via upload

---
+++ fibonachy.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int fibonachy(int x) {             
+	if (x ==0)
+		return x;
+	if (x == 1)
+		return x;
+	else
+		return fibonachy(x - 1) + fibonachy(x - 2);
+}
+
+int main() {
+	int x = 0;
+	int s = 0;
+	cout << "Введите число: " << endl;
+	cin >> x;
+	while (s < x) {
+		cout << fibonachy(s) << endl;
+		s++;
+	}
+	return 0;
+}
